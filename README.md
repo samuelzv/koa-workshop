862a0ba7701b697c637f218e9b3d9b0fa099e614
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-03-05 23:25:07 -0600

    implemented addUser endpoint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 HTTP API 
+
+If test fails you need to start mongod
--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 var app = module.exports = require('koa')();
 
+var routes = require('koa-route');
+var parse = require('co-body');
+var monk = require('monk');
+var wrap = require('co-monk');
+var db = monk('localhost/apiUsers');
+var users = wrap(db.get("users"));
+
+app.use(routes.post("/user", addUser));
+
 app.listen(3000);
 console.log('The app is listening, Port: 3000');
+
+function *addUser() {
+  console.log('hello world');
+  var userFromRequest = yield parse(this);
+
+  var insertedUser = yield users.insert(userFromRequest);
+
+  this.set("location", "/user/" + insertedUser._id);
+  this.status = 200;
+}
+
